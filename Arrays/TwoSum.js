b8e5178a3c248fdf0d6d1dc3a8be22abdf50bfe3
Avishek Kumar <avishek1999143@gmail.com>
2026-06-08 22:45:51 +0530

    Hashing_Approach

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,3 +1,24 @@
+// Time Complexity: O(n)                      Hash Set Approach  ---   Better Approach
+// Space Complexity: O(n)
+
+function twoSum(arr, target) {
+    let seen = new Set();
+
+    for (let num of arr) {
+        let complement = target - num;
+
+        if (seen.has(complement)) {
+            return true;
+        }
+
+        seen.add(num);
+    }
+
+    return false;
+}
+
+
+
 // Time Complexity: O(n^2)                      Brute Force Approach
 // Space Complexity: O(1)
 function twoSum(arr, target) {
