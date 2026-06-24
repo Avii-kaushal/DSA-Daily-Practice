ad05267c80bc13ef10f3b736263c5b8d2145b0d6
Avishek Kumar <avishek1999143@gmail.com>
2026-06-24 12:39:43 +0530

    addTwoNumbers-optimized

--- Arrays/addTwoNumbers.js
+++ Arrays/addTwoNumbers.js
@@ -1,3 +1,34 @@
+// Time Complexity: O(max(n, m))                               ---     Optimal Approach
+// Space Complexity: O(max(n, m))
+
+var addTwoNumbers = function (l1, l2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let sum = carry;
+
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+
+    return dummy.next;
+};
+
+
 // Time Complexity: O(n+m)                                ---     Brute Force
 // Space Complexity: O(n+m)
 
