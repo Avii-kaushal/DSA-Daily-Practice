7594e18d3dfe1d1f31f89b33d0a9415538f0fd0f
Avishek Kumar <avishek1999143@gmail.com>
2026-06-25 21:40:24 +0530

    MedianofTwoSortedArrays

---
+++ Arrays/MedianofTwoSortedArrays.js
@@ -0,0 +1,31 @@
+// Time Complexity: O(m + n)
+// Space Complexity: O(m + n)
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    let merged = [];
+    let i = 0, j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            merged.push(nums1[i++]);
+        } else {
+            merged.push(nums2[j++]);
+        }
+    }
+
+    while (i < nums1.length) {
+        merged.push(nums1[i++]);
+    }
+
+    while (j < nums2.length) {
+        merged.push(nums2[j++]);
+    }
+
+    const n = merged.length;
+
+    if (n % 2 === 1) {
+        return merged[Math.floor(n / 2)];
+    }
+
+    return (merged[n / 2 - 1] + merged[n / 2]) / 2;
+};
