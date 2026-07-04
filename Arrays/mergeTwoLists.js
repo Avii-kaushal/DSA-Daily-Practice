316f44e7a2511bff65d4764b244819f80f62b3ef
Avishek Kumar <avishek1999143@gmail.com>
2026-07-04 19:59:27 +0530

    mergeTwoLists-optimized

--- Arrays/mergeTwoLists.js
+++ Arrays/mergeTwoLists.js
@@ -1,3 +1,30 @@
+// Time Complexity: O(n + m)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var mergeTwoLists = function (list1, list2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    // Merge while both lists have nodes
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+
+        current = current.next;
+    }
+
+    // Attach the remaining nodes
+    current.next = list1 !== null ? list1 : list2;
+
+    return dummy.next;
+};
+
+
 // Time Complexity: O(n + m)                               ---     Brute Force
 // Space Complexity: O(n + m)
 
