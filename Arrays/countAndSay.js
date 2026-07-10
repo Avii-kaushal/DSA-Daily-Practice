8abf6ed4f66244e66dc69b4cce055930e298a2ae
Avishek Kumar <avishek1999143@gmail.com>
2026-07-10 18:17:45 +0530

    countAndSay-optimized

--- Arrays/countAndSay.js
+++ Arrays/countAndSay.js
@@ -1,4 +1,30 @@
-// Time Complexity: O(2ⁿ * m)   
+// Time Complexity: O(n * m)                               ---     Optimal Approach
+// Space Complexity: O(m)
+
+var countAndSay = function (n) {
+    let result = "1";
+
+    for (let i = 2; i <= n; i++) {
+        let current = "";
+        let count = 1;
+
+        for (let j = 1; j <= result.length; j++) {
+            if (result[j] === result[j - 1]) {
+                count++;
+            } else {
+                current += count + result[j - 1];
+                count = 1;
+            }
+        }
+
+        result = current;
+    }
+
+    return result;
+};
+
+
+// Time Complexity: O(2ⁿ * m)                               ---     Brute Force (Recursive)
 // Space Complexity: O(n + m)
 
 var countAndSay = function (n) {
