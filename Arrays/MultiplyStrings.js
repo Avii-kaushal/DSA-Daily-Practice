fb7fa3f905c9317fa77ac643a7aecc12eb3de0c2
Avishek Kumar <avishek1999143@gmail.com>
2026-07-15 11:11:49 +0530

    multiply-string

---
+++ Arrays/MultiplyStrings.js
@@ -0,0 +1,59 @@
+// Time Complexity: O(n * m + n + m) 
+// Space Complexity: O(n + m)
+
+var multiply = function (num1, num2) {
+    if (num1 === "0" || num2 === "0") {
+        return "0";
+    }
+
+    let result = "0";
+
+    // Add two numeric strings
+    function addStrings(a, b) {
+        let i = a.length - 1;
+        let j = b.length - 1;
+        let carry = 0;
+        let ans = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            let sum = carry;
+
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            ans = (sum % 10) + ans;
+            carry = Math.floor(sum / 10);
+        }
+
+        return ans;
+    }
+
+    // Multiply string with a single digit
+    function multiplyByDigit(num, digit) {
+        let carry = 0;
+        let ans = "";
+
+        for (let i = num.length - 1; i >= 0; i--) {
+            let product = (num[i] - '0') * digit + carry;
+            ans = (product % 10) + ans;
+            carry = Math.floor(product / 10);
+        }
+
+        if (carry) ans = carry + ans;
+
+        return ans;
+    }
+
+    for (let i = num2.length - 1; i >= 0; i--) {
+        let digit = num2[i] - '0';
+
+        let partial = multiplyByDigit(num1, digit);
+
+        // Append zeros according to place value
+        partial += "0".repeat(num2.length - 1 - i);
+
+        result = addStrings(result, partial);
+    }
+
+    return result;
+};
