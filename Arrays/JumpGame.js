f025cb2e9b0e2af2dcc9edfc27727246b00a4703
Avishek Kumar <avishek1999143@gmail.com>
2026-07-29 12:47:34 +0530

    jump-Game-optimized

--- Arrays/JumpGame.js
+++ Arrays/JumpGame.js
@@ -1,4 +1,30 @@
-// Time Complexity: O(n²)                      
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var jump = function (nums) {
+    let jumps = 0;
+    let currentEnd = 0;
+    let farthest = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+
+        // End of current jump range
+        if (i === currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+};
+
+
+
+
+
+
+// Time Complexity: O(n²)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var jump = function (nums) {
