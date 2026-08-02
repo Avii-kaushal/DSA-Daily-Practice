a6765dba3fe899e16ace3fbdee456d3e17b6a454
Avishek Kumar <avishek1999143@gmail.com>
2026-08-02 14:33:19 +0530

    trap-water-optimal

--- Arrays/Trapping Rain Water.js
+++ Arrays/Trapping Rain Water.js
@@ -1,4 +1,44 @@
-// Time Complexity: O(n)                              
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var trap = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+
+    let leftMax = 0;
+    let rightMax = 0;
+
+    let water = 0;
+
+    while (left < right) {
+
+        if (height[left] < height[right]) {
+
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+
+            left++;
+        } else {
+
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+
+            right--;
+        }
+    }
+
+    return water;
+};
+
+
+
+// Time Complexity: O(n)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var trap = function (height) {
