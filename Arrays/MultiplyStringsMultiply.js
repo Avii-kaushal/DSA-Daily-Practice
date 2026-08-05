dbc6e8bd6465c95afac23b9dc20cb77567b63074
Avishek Kumar <avishek1999143@gmail.com>
2026-08-05 12:47:42 +0530

    Multiply-optimal

--- Arrays/MultiplyStringsMultiply.js
+++ Arrays/MultiplyStringsMultiply.js
@@ -1,4 +1,41 @@
-// Time Complexity: O(m × n²)         
+// Time Complexity: O(m × n)                               ---     Optimal Approach
+// Space Complexity: O(m + n)
+
+var multiply = function (num1, num2) {
+    if (num1 === "0" || num2 === "0") {
+        return "0";
+    }
+
+    const m = num1.length;
+    const n = num2.length;
+    const result = new Array(m + n).fill(0);
+
+    // Multiply each digit
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+
+            let mul = (num1[i] - '0') * (num2[j] - '0');
+
+            let sum = mul + result[i + j + 1];
+
+            result[i + j + 1] = sum % 10;
+            result[i + j] += Math.floor(sum / 10);
+        }
+    }
+
+    // Remove leading zeros
+    while (result[0] === 0) {
+        result.shift();
+    }
+
+    return result.join("");
+};
+
+
+
+
+
+// Time Complexity: O(m × n²)                               ---     Brute Force
 // Space Complexity: O(m + n)
 
 var multiply = function (num1, num2) {
