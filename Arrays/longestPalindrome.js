0c09d296d8ff78dbea0bfe0413aed12a50767808
Avishek Kumar <avishek1999143@gmail.com>
2026-08-06 16:15:51 +0530

    longestPalindrome

--- Arrays/longestPalindrome.js
+++ Arrays/longestPalindrome.js
@@ -1,118 +1,29 @@
-// Time Complexity: O(n)                      Manacher's Algorithm  ---   Optimal Approach
+// Time Complexity: O(n)                     
 // Space Complexity: O(n)
 
-var longestPalindrome = function(s) {
-    let str = "^#" + s.split("").join("#") + "#$";
-    let n = str.length;
+var longestPalindrome = function (s) {
+    let map = new Map();
 
-    let p = new Array(n).fill(0);
-    let center = 0;
-    let right = 0;
+    // Count frequency of each character
+    for (let ch of s) {
+        map.set(ch, (map.get(ch) || 0) + 1);
+    }
 
-    let maxLen = 0;
-    let centerIndex = 0;
+    let length = 0;
+    let hasOdd = false;
 
-    for (let i = 1; i < n - 1; i++) {
-        let mirror = 2 * center - i;
-
-        if (i < right) {
-            p[i] = Math.min(right - i, p[mirror]);
-        }
-
-        while (str[i + 1 + p[i]] === str[i - 1 - p[i]]) {
-            p[i]++;
-        }
-
-        if (i + p[i] > right) {
-            center = i;
-            right = i + p[i];
-        }
-
-        if (p[i] > maxLen) {
-            maxLen = p[i];
-            centerIndex = i;
+    for (let count of map.values()) {
+        if (count % 2 === 0) {
+            length += count;
+        } else {
+            length += count - 1;
+            hasOdd = true;
         }
     }
 
-    let start = Math.floor((centerIndex - maxLen) / 2);
+    if (hasOdd) {
+        length += 1;
+    }
 
-    return s.substring(start, start + maxLen);
+    return length;
 };
-
-
-
-
-// Time Complexity: O(n^2)                      Expand Around Center Approach  ---   Better Approach
-// Space Complexity: O(1)
-
-var longestPalindrome = function(s) {
-    if (s.length < 2) return s;
-
-    let start = 0;
-    let maxLength = 1;
-
-    function expand(left, right) {
-        while (
-            left >= 0 &&
-            right < s.length &&
-            s[left] === s[right]
-        ) {
-            if (right - left + 1 > maxLength) {
-                maxLength = right - left + 1;
-                start = left;
-            }
-
-            left--;
-            right++;
-        }
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        // Odd length palindrome
-        expand(i, i);
-
-        // Even length palindrome
-        expand(i, i + 1);
-    }
-
-    return s.substring(start, start + maxLength);
-};
-
-
-// Time Complexity: O(n^3)                      Brute Force Approach
-// Space Complexity: O(1)
-
-var longestPalindrome = function(s) {
-    let longest = "";
-
-    function isPalindrome(str) {
-        let left = 0;
-        let right = str.length - 1;
-
-        while (left < right) {
-            if (str[left] !== str[right]) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-            let substr = s.substring(i, j + 1);
-
-            if (isPalindrome(substr) && substr.length > longest.length) {
-                longest = substr;
-            }
-        }
-    }
-
-    return longest;
-};
-
-// Driver Code
-let s = "babad";
-console.log(longestPalindrome(s));
