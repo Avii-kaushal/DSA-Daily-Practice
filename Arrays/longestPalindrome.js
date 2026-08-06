8ec1a555a443c9505f8d17bb3348a11d60f67c05
Avishek Kumar <avishek1999143@gmail.com>
2026-08-06 16:16:17 +0530

    longestPalindrome-optimal

--- Arrays/longestPalindrome.js
+++ Arrays/longestPalindrome.js
@@ -1,4 +1,29 @@
-// Time Complexity: O(n)                     
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var longestPalindrome = function (s) {
+    let set = new Set();
+    let length = 0;
+
+    for (let ch of s) {
+        if (set.has(ch)) {
+            set.delete(ch);
+            length += 2;
+        } else {
+            set.add(ch);
+        }
+    }
+
+    // One odd character can be placed in the center
+    if (set.size > 0) {
+        length += 1;
+    }
+
+    return length;
+};
+
+
+// Time Complexity: O(n)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var longestPalindrome = function (s) {
