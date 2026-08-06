9d9523b6ee9ad31c9a6a363c6371c9cb8aca0054
Avishek Kumar <avishek1999143@gmail.com>
2026-08-06 16:09:22 +0530

    Valid-Palindrone-optimal

--- Arrays/validpalindrome.js
+++ Arrays/validpalindrome.js
@@ -1,4 +1,43 @@
-// Time Complexity: O(n)                  
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var isPalindrome = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+
+        // Skip non-alphanumeric characters
+        while (left < right && !isAlphaNumeric(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphaNumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
+
+function isAlphaNumeric(ch) {
+    return (
+        (ch >= 'a' && ch <= 'z') ||
+        (ch >= 'A' && ch <= 'Z') ||
+        (ch >= '0' && ch <= '9')
+    );
+}
+
+
+
+// Time Complexity: O(n)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var isPalindrome = function (s) {
