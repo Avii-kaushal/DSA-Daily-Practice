7edd5be4e612c9d8ebfd8d1a53e480ad74e7bd90
Avishek Kumar <avishek1999143@gmail.com>
2026-08-10 12:55:11 +0530

    RemoveDuplicates-optimal

--- Arrays/Remove Duplicates from Sorted Array II.js
+++ Arrays/Remove Duplicates from Sorted Array II.js
@@ -1,4 +1,32 @@
-// Time Complexity: O(n)                             
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var removeDuplicates = function (nums) {
+    // If length is <= 2, all elements are allowed
+    if (nums.length <= 2) {
+        return nums.length;
+    }
+
+    let k = 2;
+
+    for (let i = 2; i < nums.length; i++) {
+
+        // Allow current number if it is different
+        // from the element two positions before
+        if (nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+
+
+
+
+// Time Complexity: O(n)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var removeDuplicates = function (nums) {
