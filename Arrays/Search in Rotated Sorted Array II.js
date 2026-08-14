c8e9a11ea71d889306f10800c4e0da239c230fb6
Avishek Kumar <avishek1999143@gmail.com>
2026-08-14 12:16:45 +0530

    SearchInRotatedSortedArray

---
+++ Arrays/Search in Rotated Sorted Array II.js
@@ -0,0 +1,12 @@
+// Time Complexity: O(n)                        
+// Space Complexity: O(1)
+
+var search = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return true;
+        }
+    }
+
+    return false;
+};
