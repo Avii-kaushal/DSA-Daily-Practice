e69f6a79fddafccca08406af8919e99528e1cad0
Avishek Kumar <avishek1999143@gmail.com>
2026-08-16 21:26:41 +0530

    length of last word

--- Arrays/lengthOfLastWord.js
+++ Arrays/lengthOfLastWord.js
@@ -1,4 +1,25 @@
-// Time Complexity: O(n)             
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var lengthOfLastWord = function (s) {
+    let i = s.length - 1;
+    let length = 0;
+
+    // Skip trailing spaces
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    // Count characters of the last word
+    while (i >= 0 && s[i] !== " ") {
+        length++;
+        i--;
+    }
+
+    return length;
+};
+
+// Time Complexity: O(n)                               ---     Brute Force
 // Space Complexity: O(n)
 
 var lengthOfLastWord = function (s) {
