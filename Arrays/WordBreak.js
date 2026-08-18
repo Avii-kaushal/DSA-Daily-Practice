a22ec0b2845e910a39881dcd45315a9f4b493a50
Avishek Kumar <avishek1999143@gmail.com>
2026-08-18 12:33:32 +0530

    wordBreak-optimal

--- Arrays/WordBreak.js
+++ Arrays/WordBreak.js
@@ -1,4 +1,37 @@
-// Time Complexity: O(2^n)                  
+// Time Complexity: O(n² × m)                         ---     Optimal Approach
+// Space Complexity: O(n)
+
+var wordBreak = function (s, wordDict) {
+    let wordSet = new Set(wordDict);
+
+    let dp = new Array(s.length + 1).fill(false);
+
+    // Empty string can be segmented
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; i++) {
+
+        for (let j = 0; j < i; j++) {
+
+            // Previous part is valid
+            // Current substring exists in dictionary
+            if (
+                dp[j] &&
+                wordSet.has(s.substring(j, i))
+            ) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[s.length];
+};
+
+
+
+
+// Time Complexity: O(2^n)                         ---     Brute Force
 // Space Complexity: O(n)
 
 var wordBreak = function (s, wordDict) {
