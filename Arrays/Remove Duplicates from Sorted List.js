07b9a8b99cb0af11ddf41eb8e9387c45673cb5fe
Avishek Kumar <avishek1999143@gmail.com>
2026-08-20 11:41:46 +0530

    Remoe-Duplicates-optimal

--- Arrays/Remove Duplicates from Sorted List.js
+++ Arrays/Remove Duplicates from Sorted List.js
@@ -1,4 +1,28 @@
-// Time Complexity: O(n)                            
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var deleteDuplicates = function (head) {
+    let current = head;
+
+    while (current !== null && current.next !== null) {
+
+        // If current and next have the same value
+        if (current.val === current.next.val) {
+
+            // Skip the duplicate node
+            current.next = current.next.next;
+
+        } else {
+            // Move to the next node
+            current = current.next;
+        }
+    }
+
+    return head;
+};
+
+
+// Time Complexity: O(n)                                   ---     Brute Force
 // Space Complexity: O(n)
 
 var deleteDuplicates = function (head) {
