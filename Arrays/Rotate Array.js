98290a73e6093835bd3555c3d34a093001227c65
Avishek Kumar <avishek1999143@gmail.com>
2026-08-26 11:46:34 +0530

    RotateArray-optimal

--- Arrays/Rotate Array.js
+++ Arrays/Rotate Array.js
@@ -1,4 +1,36 @@
-// Time Complexity: O(n * k)                          
+// Time Complexity: O(n)                               ---     Optimal Approach
+// Space Complexity: O(1)
+
+var rotate = function (nums, k) {
+    const n = nums.length;
+
+    // Handle k greater than array length
+    k = k % n;
+
+    function reverse(left, right) {
+        while (left < right) {
+            [nums[left], nums[right]] =
+                [nums[right], nums[left]];
+
+            left++;
+            right--;
+        }
+    }
+
+    // Reverse entire array
+    reverse(0, n - 1);
+
+    // Reverse first k elements
+    reverse(0, k - 1);
+
+    // Reverse remaining elements
+    reverse(k, n - 1);
+};
+
+
+
+
+// Time Complexity: O(n * k)                           ---     Brute Force
 // Space Complexity: O(1)
 
 var rotate = function (nums, k) {
