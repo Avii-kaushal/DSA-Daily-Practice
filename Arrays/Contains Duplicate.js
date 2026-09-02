cd5bfdd66b8d2027f2c233a9a44be35c5b7aa876
Avishek Kumar <avishek1999143@gmail.com>
2026-09-02 20:53:26 +0530

    containsDuplicate

---
+++ Arrays/Contains Duplicate.js
@@ -0,0 +1,17 @@
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+var containsDuplicate = function (nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
