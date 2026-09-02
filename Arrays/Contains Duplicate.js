3d0013d66a2fa5eda2371e85725e73d545d77b5c
Avishek Kumar <avishek1999143@gmail.com>
2026-09-02 20:53:53 +0530

    containsDuplicate-optimal

--- Arrays/Contains Duplicate.js
+++ Arrays/Contains Duplicate.js
@@ -1,3 +1,23 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+var containsDuplicate = function (nums) {
+
+    const set = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (set.has(nums[i])) {
+            return true;
+        }
+
+        set.add(nums[i]);
+    }
+
+    return false;
+};
+
+
 // Time Complexity: O(n²)
 // Space Complexity: O(1)
 
