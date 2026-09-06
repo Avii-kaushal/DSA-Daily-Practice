49c6dac2f21ce5b05d7e112b196ccd93339cecc4
Avishek Kumar <avishek1999143@gmail.com>
2026-09-06 23:23:20 +0530

    Solve Set Matrix Zeroes problem-optimizedWay

--- Arrays/SetMatrixZeroes.js
+++ Arrays/SetMatrixZeroes.js
@@ -1,3 +1,74 @@
+// Time Complexity: O(m × n)                 First Row & Column Markers  ---   Optimal Approach
+// Space Complexity: O(1)
+
+function setZeroes(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let firstRowZero = false;
+    let firstColZero = false;
+
+    // Check first row
+    for (let j = 0; j < cols; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZero = true;
+        }
+    }
+
+    // Check first column
+    for (let i = 0; i < rows; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+        }
+    }
+
+    // Mark rows and columns
+    for (let i = 1; i < rows; i++) {
+        for (let j = 1; j < cols; j++) {
+
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Set marked rows to 0
+    for (let i = 1; i < rows; i++) {
+        if (matrix[i][0] === 0) {
+            for (let j = 1; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Set marked columns to 0
+    for (let j = 1; j < cols; j++) {
+        if (matrix[0][j] === 0) {
+            for (let i = 1; i < rows; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Set first row to 0
+    if (firstRowZero) {
+        for (let j = 0; j < cols; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Set first column to 0
+    if (firstColZero) {
+        for (let i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+
+
+
 // Time Complexity: O(m × n)                 Row & Column Array Approach  ---   Better Approach
 // Space Complexity: O(m + n)
 
