e0c2e83998c9dfb0bff5a7d9b84aa636ff20cbf8
Avishek Kumar <avishek1999143@gmail.com>
2026-09-06 23:22:51 +0530

    Solve Set Matrix Zeroes problem-betterWay

--- Arrays/SetMatrixZeroes.js
+++ Arrays/SetMatrixZeroes.js
@@ -1,3 +1,47 @@
+// Time Complexity: O(m × n)                 Row & Column Array Approach  ---   Better Approach
+// Space Complexity: O(m + n)
+
+function setZeroes(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let zeroRows = new Array(rows).fill(false);
+    let zeroCols = new Array(cols).fill(false);
+
+    // Find rows and columns containing 0
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+
+            if (matrix[i][j] === 0) {
+                zeroRows[i] = true;
+                zeroCols[j] = true;
+            }
+        }
+    }
+
+    // Set rows to 0
+    for (let i = 0; i < rows; i++) {
+        if (zeroRows[i]) {
+            for (let j = 0; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Set columns to 0
+    for (let j = 0; j < cols; j++) {
+        if (zeroCols[j]) {
+            for (let i = 0; i < rows; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+
+
+
 // Time Complexity: O(m × n × (m + n))       Brute Force Approach
 // Space Complexity: O(m × n)
 
