bfa100dbf001b6a7fdc94689b0b19be7cb3b0939
Avishek Kumar <avishek1999143@gmail.com>
2026-09-08 11:44:55 +0530

    Longest Consecutive

--- Arrays/longestConsecutive.js
+++ Arrays/longestConsecutive.js
@@ -1,3 +1,34 @@
+// Time Complexity: O(n)                      Hash Set Approach  ---   Optimal Approach
+// Space Complexity: O(n)
+
+function longestConsecutive(nums) {
+    let set = new Set(nums);
+    let longest = 0;
+
+    for (let num of set) {
+
+        // Check if num is the start of a sequence
+        if (!set.has(num - 1)) {
+
+            let current = num;
+            let count = 1;
+
+            // Count consecutive numbers
+            while (set.has(current + 1)) {
+                current++;
+                count++;
+            }
+
+            longest = Math.max(longest, count);
+        }
+    }
+
+    return longest;
+}
+
+
+
+
 // Time Complexity: O(n^2)                      Brute Force Approach
 // Space Complexity: O(1)
 
