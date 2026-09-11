7080ce4523e4ea0593dda3674b6a0ac7f8f05108
Avishek Kumar <avishek1999143@gmail.com>
2026-09-11 23:37:40 +0530

    ClimbingStairs

--- Arrays/climbStairs.js
+++ Arrays/climbStairs.js
@@ -1,3 +1,27 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)                 Optimal Approach
+
+function climbStairs(n) {
+    let dp = new Array(n + 1);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+// Driver Code
+let n = 3;
+
+console.log(climbStairs(n));
+// 3
+
+
+
 // Time Complexity: O(2^n)
 // Space Complexity: O(n)                 Brute Force Approach
 
