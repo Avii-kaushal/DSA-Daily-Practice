cec6ebc64dfa248289573c6a30116b0ee2a3f80e
Avishek Kumar <avishek1999143@gmail.com>
2026-09-18 23:40:10 +0530

    insertInterval

---
+++ Arrays/Insert Interval.js
@@ -0,0 +1,46 @@
+// Time Complexity: O(n log n)              Sort + Merge  ---   Brute Force Approach
+// Space Complexity: O(n)
+
+function insert(intervals, newInterval) {
+
+    // Add the new interval
+    intervals.push(newInterval);
+
+    // Sort intervals by starting point
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let result = [];
+
+    for (let interval of intervals) {
+
+        // If result is empty OR
+        // current interval does not overlap
+        if (
+            result.length === 0 ||
+            result[result.length - 1][1] < interval[0]
+        ) {
+            result.push(interval);
+        }
+
+        // Overlapping intervals
+        else {
+            result[result.length - 1][1] =
+                Math.max(
+                    result[result.length - 1][1],
+                    interval[1]
+                );
+        }
+    }
+
+    return result;
+}
+
+
+// Driver Code
+let intervals = [[1, 3], [6, 9]];
+let newInterval = [2, 5];
+
+console.log(insert(intervals, newInterval));
+
+// Output:
+// [[1, 5], [6, 9]]
