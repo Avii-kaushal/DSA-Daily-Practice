155ca458ec3e951754ab4bb8e3f76357b6518e71
Avishek Kumar <avishek1999143@gmail.com>
2026-09-18 23:40:38 +0530

    insertInterval-optimal

--- Arrays/Insert Interval.js
+++ Arrays/Insert Interval.js
@@ -1,3 +1,58 @@
+// Time Complexity: O(n)              Single Pass  ---   Optimal Approach
+// Space Complexity: O(n)
+
+function insert(intervals, newInterval) {
+
+    let result = [];
+
+    let i = 0;
+    let n = intervals.length;
+
+    // 1. Add intervals that are completely
+    // before the new interval
+    while (
+        i < n &&
+        intervals[i][1] < newInterval[0]
+    ) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    // 2. Merge all overlapping intervals
+    while (
+        i < n &&
+        intervals[i][0] <= newInterval[1]
+    ) {
+        newInterval[0] = Math.min(
+            newInterval[0],
+            intervals[i][0]
+        );
+
+        newInterval[1] = Math.max(
+            newInterval[1],
+            intervals[i][1]
+        );
+
+        i++;
+    }
+
+    // Add the merged new interval
+    result.push(newInterval);
+
+    // 3. Add remaining intervals
+    // that are completely after newInterval
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
+}
+
+
+
+
+
 // Time Complexity: O(n log n)              Sort + Merge  ---   Brute Force Approach
 // Space Complexity: O(n)
 
