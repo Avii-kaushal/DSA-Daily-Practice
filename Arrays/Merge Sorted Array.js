13267ddeeb9924ebe64ca21ba435440401bad1d3
Avishek Kumar <avishek1999143@gmail.com>
2026-09-20 20:35:29 +0530

    MergeSortedArray-optimal

--- Arrays/Merge Sorted Array.js
+++ Arrays/Merge Sorted Array.js
@@ -1,3 +1,39 @@
+// Time Complexity: O(m + n)              Three Pointers  ---   Optimal Approach
+// Space Complexity: O(1)
+
+function merge(nums1, m, nums2, n) {
+
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    // Compare elements from the end
+    while (i >= 0 && j >= 0) {
+
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+
+        k--;
+    }
+
+    // If nums2 still has elements,
+    // copy them into nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+
+
+
+
 // Time Complexity: O((m + n) log(m + n))     Sort Approach  ---   Brute Force Approach
 // Space Complexity: O(m + n)
 
